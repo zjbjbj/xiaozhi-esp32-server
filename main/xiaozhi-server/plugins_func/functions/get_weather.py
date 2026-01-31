cb69b4569708985b94137f0ea55f57cba304851f
冷兴孜 <lengxingzi@ztoky.cn>
2026-01-31 19:14:18 +0800

    weather_config

--- main/xiaozhi-server/plugins_func/functions/get_weather.py
+++ main/xiaozhi-server/plugins_func/functions/get_weather.py
@@ -159,8 +159,8 @@ def get_weather(conn, location: str = None, lang: str = "zh_CN"):
     from core.utils.cache.manager import cache_manager, CacheType
 
     weather_config = conn.config.get("plugins", {}).get("get_weather", {})
-    api_host = weather_config.get("api_host", "mj7p3y7naa.re.qweatherapi.com")
-    api_key = weather_config.get("api_key", "a861d0d5e7bf4ee1a83d9a9e4f96d4da")
+    api_host = weather_config.get("api_host", "n55khvy9f4.re.qweatherapi.com")
+    api_key = weather_config.get("api_key", "c6e1f3d4540d469d993c03fa794ad9a5")
     default_location = weather_config.get("default_location", "广州")
     client_ip = conn.client_ip
 
